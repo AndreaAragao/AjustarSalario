8669852c6c20447a9c0a3a717346c4caa7a4ed78
Andréa <andreamaragao@gmail.com>
2023-03-13 22:37:50 -0300

    Adicionar arquivos de projeto.

---
+++ App/AjustarSalario.cs
@@ -0,0 +1,23 @@
+﻿namespace Projeto05.App
+{
+    public class AjustarSalario
+    {
+        public string NomeFuncionario { get; private set; }
+        public double Salario { get; private set; }
+
+        public AjustarSalario(string nomeQueVeio, double salarioQueVeio )
+        {
+            NomeFuncionario = nomeQueVeio;
+            Salario = salarioQueVeio;
+        }
+
+        public double novoSalario()
+        {
+            if ( Salario < 1500)
+                return Salario * 1.1;
+            return Salario * 1.05;
+
+            //return (Salario < 1500) ? Salario * 1.1 : Salario 1.05;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Projeto05.App;
+using System;
+
+namespace Projeto05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AjustarSalario pessoa1 = new AjustarSalario("Douglas", 1400);
+
+            AjustarSalario pessoa2 = new AjustarSalario("Andréa", 1600);
+            double novoSalario1 = pessoa1.novoSalario();
+            double novoSalario2 = pessoa2.novoSalario();
+
+            Console.WriteLine(pessoa1.NomeFuncionario + " R$ " + novoSalario1);
+
+            Console.WriteLine(pessoa2.NomeFuncionario + " R$ " + novoSalario2);
+          
+        }
+    }
+}
